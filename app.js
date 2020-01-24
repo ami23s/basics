f263e2143345cffac050ff1d78dd75df379e26b5
ami23s <56482154+ami23s@users.noreply.github.com>
2020-01-24 22:03:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+var path=require("path");
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",function(req,res){
+  res
+    .status(200)
+    .sendFile(path.join(__dirname,"public","index.html"));
+})
+
+
+app.listen(3000, function(){
+    console.log("Hello");
+});
